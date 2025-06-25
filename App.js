c9c21db78245c297eabec7447521da0222c9fe5c
Henry <manojlostanarevic@gmail.com>
2025-06-25 10:21:41 +0200

    header done

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import Header from './Header'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.root}>
+        <View style={styles.statusBarBackground} />
+        <StatusBar style="light" translucent />
+        <SafeAreaView>
+          <Header/>
+        </SafeAreaView>
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: '#000', 
+  },
+  statusBarBackground: {
+    height: StatusBar.currentHeight || 24, 
+    backgroundColor: '#000', 
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ Header.jsx
@@ -0,0 +1,50 @@
+import { View, StyleSheet, Text, TouchableOpacity, ImageBackground } from "react-native";
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { widthPercentageToDPas as wp } from "react-native-responsive-screen";
+import img from "./fotografije/ikon.png"
+
+
+
+function Header() {
+  return(
+  <View style={styles.header}>
+    <Text style={styles.text}>Mesindžer
+      
+    </Text>
+    <TouchableOpacity style={styles.button}>
+      <ImageBackground
+        source={img}
+        style={styles.image}
+      >
+      </ImageBackground>
+    </TouchableOpacity>
+  </View>)
+}
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: 'red',
+    height: hp('7%'),
+    width: '100%',
+    position: 'absolute',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 16
+  },
+  text: {
+    fontSize:  hp('7%') * 0.6,
+    color: 'white',
+    flexDirection: 'row'
+  },
+  button: {
+    width: hp('4.5%'),
+    height: hp('4.5%'),
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  }
+})
+
+export default Header
